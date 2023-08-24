81c61bdffb3e560230d21827c7172dba92207fd1
Yusen(NG) Wu <90329634+RayNG123@users.noreply.github.com>
2023-08-25 02:37:16 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import plotly.express as px
+
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
